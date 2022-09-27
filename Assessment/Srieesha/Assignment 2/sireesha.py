ea35ce8f87e7117883caff23086df59d2485fcbc
vandanareddy12 <113964206+vandanareddy12@users.noreply.github.com>
2022-09-27 09:49:58 +0530

    Add files via upload

---
+++ Assessment/Srieesha/Assignment 2/sireesha.py
@@ -0,0 +1,288 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/Administrator/AppData/Local/Programs/Python/Python310/sireeshaibm.py
+Temperature is Normal
+Alert Temperature is high ! !
+Alert Temperature is high ! !
+Alert Temperature is high ! !
+Alert Temperature is high ! !
+Alert Temperature is high ! !
+Alert Temperature is high ! !
+Temperature is Normal
+Temperature is Normal
+Alert Temperature is high ! !
+Temperature is Normal
+Temperature is Normal
+Temperature is Normal
+Alert Temperature is high ! !
+Temperature is Normal
+Temperature is Normal
+Temperature is Normal
+Temperature is Normal
+Temperature is Normal
+Temperature is Normal
+Alert Temperature is high ! !
+Temperature is Normal
+Alert Temperature is high ! !
+Alert Temperature is high ! !
+Alert Temperature is high ! !
+Alert Temperature is high ! !
+Alert Temperature is high ! !
+Alert Temperature is high ! !
+Temperature is Normal
+Alert Temperature is high ! !
+Temperature is Normal
+Alert Temperature is high ! !
+Alert Temperature is high ! !
+Temperature is Normal
+Alert Temperature is high ! !
+Temperature is Normal
+Temperature is Normal
+Alert Temperature is high ! !
+Temperature is Normal
+Temperature is Normal
+Temperature is Normal
+Temperature is Normal
+Alert Temperature is high ! !
+Alert Temperature is high ! !
+Temperature is Normal
+Temperature is Normal
+Temperature is Normal
+Temperature is Normal
+Alert Temperature is high ! !
+Temperature is Normal
+Temperature is Normal
+Temperature is Normal
+Alert Temperature is high ! !
+Alert Temperature is high ! !
+Temperature is Normal
+Temperature is Normal
+Temperature is Normal
+Temperature is Normal
+Alert Temperature is high ! !
+Temperature is Normal
+Alert Temperature is high ! !
+Temperature is Normal
+Alert Temperature is high ! !
+Temperature is Normal
+Alert Temperature is high ! !
+Temperature is Normal
+Temperature is Normal
+Temperature is Normal
+Alert Temperature is high ! !
+Temperature is Normal
+Alert Temperature is high ! !
+Alert Temperature is high ! !
+Alert Temperature is high ! !
+Alert Temperature is high ! !
+Alert Temperature is high ! !
+Temperature is Normal
+Alert Temperature is high ! !
+Temperature is Normal
+Temperature is Normal
+Temperature is Normal
+Temperature is Normal
+Alert Temperature is high ! !
+Alert Temperature is high ! !
+Temperature is Normal
+Alert Temperature is high ! !
+Temperature is Normal
+Alert Temperature is high ! !
+Alert Temperature is high ! !
+Alert Temperature is high ! !
+Temperature is Normal
+Temperature is Normal
+Alert Temperature is high ! !
+Alert Temperature is high ! !
+Temperature is Normal
+Alert Temperature is high ! !
+Temperature is Normal
+Temperature is Normal
+Alert Temperature is high ! !
+Alert Temperature is high ! !
+Temperature is Normal
+Alert Temperature is high ! !
+Alert Temperature is high ! !
+Alert Temperature is high ! !
+Temperature is Normal
+Temperature is Normal
+Alert Temperature is high ! !
+Temperature is Normal
+Alert Temperature is high ! !
+Alert Temperature is high ! !
+Alert Temperature is high ! !
+Temperature is Normal
+Alert Temperature is high ! !
+Temperature is Normal
+Temperature is Normal
+Temperature is Normal
+Temperature is Normal
+Alert Temperature is high ! !
+Temperature is Normal
+Alert Temperature is high ! !
+Alert Temperature is high ! !
+Temperature is Normal
+Temperature is Normal
+Temperature is Normal
+Alert Temperature is high ! !
+Temperature is Normal
+Temperature is Normal
+Alert Temperature is high ! !
+Alert Temperature is high ! !
+Temperature is Normal
+Alert Temperature is high ! !
+Temperature is Normal
+Temperature is Normal
+Alert Temperature is high ! !
+Alert Temperature is high ! !
+Temperature is Normal
+Temperature is Normal
+Alert Temperature is high ! !
+Alert Temperature is high ! !
+Temperature is Normal
+Temperature is Normal
+Temperature is Normal
+Temperature is Normal
+Alert Temperature is high ! !
+Temperature is Normal
+Temperature is Normal
+Alert Temperature is high ! !
+Alert Temperature is high ! !
+Temperature is Normal
+Temperature is Normal
+Temperature is Normal
+Alert Temperature is high ! !
+Temperature is Normal
+Alert Temperature is high ! !
+Temperature is Normal
+Temperature is Normal
+Alert Temperature is high ! !
+Alert Temperature is high ! !
+Temperature is Normal
+Temperature is Normal
+Temperature is Normal
+Temperature is Normal
+Alert Temperature is high ! !
+Temperature is Normal
+Temperature is Normal
+Temperature is Normal
+Alert Temperature is high ! !
+Temperature is Normal
+Alert Temperature is high ! !
+Alert Temperature is high ! !
+Temperature is Normal
+Alert Temperature is high ! !
+Alert Temperature is high ! !
+Temperature is Normal
+Temperature is Normal
+Temperature is Normal
+Alert Temperature is high ! !
+Temperature is Normal
+Temperature is Normal
+Temperature is Normal
+Alert Temperature is high ! !
+Alert Temperature is high ! !
+Temperature is Normal
+Temperature is Normal
+Temperature is Normal
+Alert Temperature is high ! !
+Alert Temperature is high ! !
+Temperature is Normal
+Temperature is Normal
+Temperature is Normal
+Alert Temperature is high ! !
+Alert Temperature is high ! !
+Alert Temperature is high ! !
+Temperature is Normal
+Alert Temperature is high ! !
+Temperature is Normal
+Temperature is Normal
+Temperature is Normal
+Temperature is Normal
+Alert Temperature is high ! !
+Alert Temperature is high ! !
+Temperature is Normal
+Temperature is Normal
+Temperature is Normal
+Temperature is Normal
+Alert Temperature is high ! !
+Alert Temperature is high ! !
+Alert Temperature is high ! !
+Temperature is Normal
+Temperature is Normal
+Temperature is Normal
+Temperature is Normal
+Temperature is Normal
+Temperature is Normal
+Alert Temperature is high ! !
+Alert Temperature is high ! !
+Temperature is Normal
+Alert Temperature is high ! !
+Alert Temperature is high ! !
+Alert Temperature is high ! !
+Temperature is Normal
+Temperature is Normal
+Temperature is Normal
+Alert Temperature is high ! !
+Alert Temperature is high ! !
+Temperature is Normal
+Alert Temperature is high ! !
+Temperature is Normal
+Alert Temperature is high ! !
+Temperature is Normal
+Temperature is Normal
+Alert Temperature is high ! !
+Temperature is Normal
+Temperature is Normal
+Alert Temperature is high ! !
+Alert Temperature is high ! !
+Temperature is Normal
+Temperature is Normal
+Temperature is Normal
+Temperature is Normal
+Temperature is Normal
+Temperature is Normal
+Temperature is Normal
+Temperature is Normal
+Alert Temperature is high ! !
+Temperature is Normal
+Alert Temperature is high ! !
+Alert Temperature is high ! !
+Temperature is Normal
+Alert Temperature is high ! !
+Temperature is Normal
+Temperature is Normal
+Temperature is Normal
+Temperature is Normal
+Temperature is Normal
+Temperature is Normal
+Temperature is Normal
+Alert Temperature is high ! !
+
+========== RESTART: C:/Users/Administrator/Desktop/siri/sireeshaibm.py =========
+Temperature is Normal
+Temperature is Normal
+Alert Temperature is high ! !
+Temperature is Normal
+Temperature is Normal
+Temperature is Normal
+Alert Temperature is high ! !
+Temperature is Normal
+Temperature is Normal
+Temperature is Normal
+Temperature is Normal
+Alert Temperature is high ! !
+Alert Temperature is high ! !
+Temperature is Normal
+Alert Temperature is high ! !
+Temperature is Normal
+Temperature is Normal
+Temperature is Normal
+Temperature is Normal
+Temperature is Normal
+Temperature is Normal
+Temperature is Normal
+Alert Temperature is high ! !
+Temperature is Normal
+Temperature is Normal
